ad0df14e199935e3b3edb811bcb55b8295a39248
Mowang <mw@loli.wang>
2024-07-31 09:04:30 +0800

    test
    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,32 +3,26 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+const myTheme = {
+  name: 'my-theme',
+  settings: [
+    {
+      scope: ['comment'],
+      settings: {
+        foreground: '#888'
+      }
+    },
+  ]
+}
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com', 
   markdown: {
     shikiConfig: {
-      // 选择 Shiki 内置的主题（或添加你自己的主题）
-      // https://shiki.style/themes
-      theme: 'dracula',
-      // 另外，也提供了多种主题
-      // 查看下面关于使用亮/暗双主题的的说明
-      themes: {
-        light: 'vitesse-dark',
-      },
-      // 禁用默认配色
-      // https://shiki.style/guide/dual-themes#without-default-color
-      // (添加于 v4.12.0)
-      defaultColor: false,
-      // 添加自定义语言
-      // 注意：Shiki 内置了无数语言，包括 .astro！
-      // https://shiki.style/languages
-      langs: [],
-      // 启用自动换行，以防止水平滚动
-      wrap: true,
-      // 添加自定义转换器：https://shiki.style/guide/transformers
-      // 查找常用转换器：https://shiki.style/packages/transformers
-      transformers: [],
+      theme: 'vitesse-light',
+      transformers: [
+        myTransformer
+      ]
     },
   },
   integrations: [mdx(), sitemap(), tailwind()]
