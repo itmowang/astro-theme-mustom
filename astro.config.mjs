4f8e58e9126f2710e685b67f1d422b666420f532
Mowang <mw@loli.wang>
2024-08-02 14:49:11 +0800

    📃 docs: 发布测试

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com', 
+  site: 'https://blog5.loli.wang', 
   outDir: "docs",
   markdown: {
     shikiConfig: {
